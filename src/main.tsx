edd276405fb30a24fc4b142e8dea683de8418c84
Denis Shulga <sulgadenis4@gmail.com>
2025-10-15 01:53:24 +0300

    chanches

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App";
+import { HashRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root") as HTMLDivElement).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </QueryClientProvider>
 );
